78a4e178588e84b71e73120c35b9b17f889a3cd4
RudraVashi <rvashi@trinity.edu>
2025-03-23 16:44:28 -0500

    Database setup complete

---
+++ database/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    waitForConnections: true,
+    connectionLimit: 10
+});
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("Connected to MySQL database");
+        connection.release();
+    }
+});
+
+module.exports = pool.promise();
