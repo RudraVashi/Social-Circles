c4d2743003fd0a903c1bb10e3594b15d1915446a
huang-el <ehuang1@trinity.edu>
2025-04-28 09:53:04 -0500

    removed app listen

--- app.js
+++ app.js
@@ -111,9 +111,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
