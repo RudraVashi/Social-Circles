65a651d556740ee7cb1a75e4549da942dd1dcc28
Zach Weiss <zweiss1@trinity.edu>
2025-05-06 16:19:48 -0500

    Username is listed on home page, images no longer have backgrounds

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Social Circles' });
+  // renderInfo = { title: 'Social Circles' };
+  // if (req.session.user){ // If the user is logged in, render with their info (so we can show their username and such)
+  //   renderInfo["user"] = req.session.user;
+  // }
+  res.render('index', { title: 'Social Circles', user: req.session.user});
 });
 
 router.get('/login', function(req, res, next) {
